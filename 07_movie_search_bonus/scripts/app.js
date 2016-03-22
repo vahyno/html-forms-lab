c73af7eb4ef4621aafba727fd87ff59744a13e9e
nathanallen <thenathanator@gmail.com>
2016-03-22 13:55:42 -0700

    solution 7 bonus

--- 07_movie_search_bonus/scripts/app.js
+++ 07_movie_search_bonus/scripts/app.js
@@ -2,6 +2,22 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-  // code in here
+  $("form").submit(function(event){
+    event.preventDefault(); // stop the form from submitting
+    var form_data = $(this).serialize(); // "s=avatar&year=2009"
+
+    $.ajax({
+      method: "GET",
+      url: "http://www.omdbapi.com",
+      data: form_data,
+      success: renderJsonResponseToPage
+    })
+
+  })
+
+  function renderJsonResponseToPage(response_json){
+    var formatted_json = JSON.stringify(response_json, true, 4)
+    $("pre").text(formatted_json)
+  }
 
 });
