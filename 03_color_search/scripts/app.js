973d887b3e14a13c0fb3ce7d2d6bbe6e9fb6d4dc
nathanallen <thenathanator@gmail.com>
2016-03-22 12:29:59 -0700

    solution 3 color picker

--- 03_color_search/scripts/app.js
+++ 03_color_search/scripts/app.js
@@ -2,6 +2,8 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-  // code in here
+  $("input[type=color]").change(function(){
+    $("button").css("background-color", $(this).val())
+  })
 
 });
